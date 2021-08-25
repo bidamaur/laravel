dfe0ae782647ea62704e4574e1d4929bc93780fe
bidamaur <36158975+bidamaur@users.noreply.github.com>
2021-08-25 09:42:55 +0200

    Add About page

---
+++ My_tuto/resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ABOUT-US</title>
+
+    </head>
+    <body >
+ <center>
+    <h1 >TO KNOW MORE ABOUT US  &middot;</h1>
+<p><small>this is a time <a href='/'>Home</a></small></p>    
+</center>
+<footer>&copy; DEVMY Root</footer>
+    </body>
+</html>
--- My_tuto/routes/console.php
+++ My_tuto/routes/console.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Artisan::command('score:version', function () {
+    $this->comment("Version V1.0.1");
+})->purpose('application version');
+
+Artisan::command('score:licence', function () {
+    $this->comment("GLT");
+})->purpose('application version');
